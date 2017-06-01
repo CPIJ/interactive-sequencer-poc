c95a2a4c1e35701a02b81de87d1b951fcabae706
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-06-01 10:47:32 +0200

    made a way to syncronise time

---
+++ time-machine.js
@@ -0,0 +1,26 @@
+class TimeMachine {
+    constructor(bpm) {
+        this.interval = 60000 / bpm
+        this.isPlaying = false
+    }
+
+    start() {
+        this.isPlaying = true
+        this.onTick(this.event)
+    }
+
+    stop() {
+        this.isPlaying = false
+    }
+
+    onTick(event) {
+        this.event = event
+        if (!this.isPlaying) return
+
+        this.event()
+
+        setTimeout(() => {
+            this.onTick(event)
+        }, this.interval);
+    }
+}
