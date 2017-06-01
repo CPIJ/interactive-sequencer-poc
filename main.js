1cdc8f4f4d7cbcf8c393745735bb96aade73a98a
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-06-01 11:28:02 +0200

    Style changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const bpm = 128;
+const BPM = 128;
 let timeMachine
 
 var instrumentGroups = []
@@ -8,7 +8,7 @@ var snarePattern = [NONE, DRUMS.SNARE, NONE, DRUMS.SNARE, NONE, DRUMS.SNARE, NON
 
 function preload() {
     this.instrumentGroups.push(new InstrumentGroup([kickPattern, snarePattern]))
-    this.timeMachine = new TimeMachine(bpm)
+    this.timeMachine = new TimeMachine(BPM)
     tracker = new Tracker(document.getElementById('myVideo'))
     VideoPlayer.setup(document.getElementById('myVideo'))
 }
@@ -16,11 +16,15 @@ function preload() {
 function setup() {
     createCanvas(500, 500)
 
+    // This function is called when an object is tracked.
     tracker.onTrack(e => {
+
         clear()
         rect(e.x, e.y, e.width, e.height)
+
     })
 
+    // This function is called when time ticks. (based on bpm)
     this.timeMachine.onTick(() => {
 
         this.instrumentGroups.forEach((instrumentGroup) => {
